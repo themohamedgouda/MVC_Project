8b4ac736a1cb045bea8262144bcaaaf942507ae7
Mohamed Gouda <themohamedgouda@gmail.com>
2025-04-08 23:48:24 +0200

    Part 04 Department Controller Create

--- BusinessLogic/DataTransfereObjects/CreatedDepartmentDTO.cs
+++ BusinessLogic/DataTransfereObjects/CreatedDepartmentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace BusinessLogic.DataTransfereObjects
 {
     public class CreatedDepartmentDTO
     {
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
+        [Required]
+        [Range(0,100)]
         public string Code { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
         public DateOnly DateOfCreation { get; set; }
--- Presentation/Controllers/DepartmentController.cs
+++ Presentation/Controllers/DepartmentController.cs
@@ -1,16 +1,70 @@
-﻿using BusinessLogic.Services;
+﻿using BusinessLogic.DataTransfereObjects;
+using BusinessLogic.Services;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
-    public class DepartmentController(IDepartmentServices departmentservices) : Controller
+    public class DepartmentController(IDepartmentServices _departmentservices , 
+        ILogger<DepartmentController> _logger , IWebHostEnvironment _environment) : Controller
     {
 
         public IActionResult Index()
         
         {
-            var departments = departmentservices.GetAllDepartments();
+            var departments = _departmentservices.GetAllDepartments();
             return View(departments);
         }
+
+        #region CreateDepartment
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDTO DepartmentDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _departmentservices.AddDepartment(DepartmentDTO);
+                    if (Result > 0)
+                    {
+                       // return View(nameof(Index));
+                       return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department Can't be Created");
+                      //  return View(DepartmentDTO) ;
+                    }
+                }
+                catch(Exception Ex)
+                {
+                    if (_environment.IsDevelopment())
+                    {
+                        //Dev
+                        ModelState.AddModelError(string.Empty, $"{Ex.Message}");
+                      //  return View(DepartmentDTO);
+                    }
+                    else
+                    {
+                        //Deployment
+                        _logger.LogError(Ex.Message);
+                       // return View(DepartmentDTO);
+                    }
+
+                }
+            }
+            //else
+            //{
+            //    return View(DepartmentDTO);
+
+            //}
+            return View(DepartmentDTO);
+        }
+        #endregion
     }
 }
