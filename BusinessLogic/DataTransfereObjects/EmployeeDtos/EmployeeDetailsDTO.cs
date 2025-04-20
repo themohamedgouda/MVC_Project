9f1b8da9aa657be1794831b796a912ff304e4696
Mohamed Gouda <themohamedgouda@gmail.com>
2025-04-20 03:40:28 +0200

    Part 05 Refactor CreateEmployee Action

--- BusinessLogic/DataTransfereObjects/EmployeeDtos/EmployeeDetailsDTO.cs
+++ BusinessLogic/DataTransfereObjects/EmployeeDtos/EmployeeDetailsDTO.cs
@@ -10,7 +10,7 @@ namespace BusinessLogic.DataTransfereObjects.EmployeeDtos
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public int? Age { get; set; }
+        public int Age { get; set; }
         public string? Address { get; set; }
         public decimal Salary { get; set; }
         public bool IsActive { get; set; }
